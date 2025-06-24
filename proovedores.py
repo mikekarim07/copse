86f457f6aa11178272d0c6ef84678985e280cca4
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2025-06-24 13:06:11 -0600

    Update proovedores.py

--- proovedores.py
+++ proovedores.py
@@ -1,3 +1,16 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="COPSE - Clientes",
+    page_icon="📈",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'mailto:miguel.karim@karimortega.com'
+    }
+)
+
+st.image("logo kalata.png", width=120)
 st.title("Modulo de Proovedores")
+
+
